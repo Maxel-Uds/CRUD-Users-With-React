38d6ef80efbb314467ea0a519f3e52e5311c12d3
Maxel-Uds <maxellopes32@gmail.com>
2022-06-29 14:38:47 -0300

    Componente Logo

--- frontend/src/components/templates/Logo.jsx
+++ frontend/src/components/templates/Logo.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Logo from '../../assets/imgs/logoReact.png';
 import './Logo.css';
 
 export default function logo(props) {
     return (
         <aside className="logo">
-            Logo
+            <img src={Logo} alt="Logo do React"/>
         </aside>
     );
 }
