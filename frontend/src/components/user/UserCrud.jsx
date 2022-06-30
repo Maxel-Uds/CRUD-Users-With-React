f620f97bc8e7f52d46c53a151152163f96ea8464
Maxel-Uds <maxellopes32@gmail.com>
2022-06-29 21:48:22 -0300

    Componente Cadastro de Usuário parte 2

--- frontend/src/components/user/UserCrud.jsx
+++ frontend/src/components/user/UserCrud.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Axios from 'axios';
 import Main from '../templates/Main';
 
 const headerProps = {
@@ -7,7 +8,38 @@ const headerProps = {
     subtitle: 'Cadastro de Usuários: Incluir, Listar, Alterar e Deletar'
 }
 
+const baseUrl = 'http://localhost:8080/users';
+
+const initialState = {
+    user: { name: '', email: '' },
+    list: []
+}
+
 export default class UserCrud extends Component {
+
+    state = { ...initialState };
+
+    clear() {
+        this.setState({ user: initialState.user });
+    }
+
+    save() {
+        const user = this.state.user;
+        const method = user.id ? 'put' : 'post';
+        const url = user.id ? `${baseUrl}/${user.id}` : baseUrl;
+
+        Axios[method](url, user)
+        .then(response => {
+            const list = this.getUdatedList(response.data);
+            this.setState({ user: initialState.user, list });
+        });
+    }
+
+    getUdatedList(user) {
+        const list = this.state.list.filter(u => u.id !== user.id);
+        list.unshift(user); //Coloca determinado elemento na primeira posição do array
+        return list;
+    }
 
     render() {
         return (
